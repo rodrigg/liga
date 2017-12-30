023237ccaa23d604d37c96cb590d8cdab5af4c84
Alvaro <alvaro.rodriguez.miguel@gmail.com>
2017-12-30 15:40:44 +0100

    Commit inicial

---
+++ proyectoLiga3/src/main/java/com/alvaro/proyectoLiga3/ProyectoLiga3Application.java
@@ -0,0 +1,12 @@
+package com.alvaro.proyectoLiga3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoLiga3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoLiga3Application.class, args);
+	}
+}
---
+++ proyectoLiga3/src/test/java/com/alvaro/proyectoLiga3/ProyectoLiga3ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alvaro.proyectoLiga3;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProyectoLiga3ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
